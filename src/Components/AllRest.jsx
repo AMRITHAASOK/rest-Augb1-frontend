0b5ede9eb3cf2de439d3ca40e99b507b1b754479
amrithaasok <amrithaasok.aa@gamil.com>
2023-12-27 12:27:47 +0530

    restaurant-listing-frontend

---
+++ src/Components/AllRest.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Row ,Col} from 'react-bootstrap'
+import RestCard from './RestCard'
+function AllRest() {
+
+  const base_url='https://rest-augb1.onrender.com/restaurants'
+  //to hold array of restaurant data
+  const [AllRestData,setAllRestData] = useState([])
+
+  //API fetching
+  const fetchData=async()=>{
+      const result = await axios.get(base_url)
+      console.log(result.data);//array(10)
+      setAllRestData(result.data)
+  }
+
+  console.log(AllRestData);//array(10)
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  return (
+    <div>
+      <Row>
+          {
+            AllRestData.map(item=>(
+              <Col sm={12} md={6} lg={4} xl={3}>
+              <RestCard restarants={item}/>
+              </Col>
+            ))
+          }
+      </Row>
+    </div>
+  )
+}
+
+export default AllRest
